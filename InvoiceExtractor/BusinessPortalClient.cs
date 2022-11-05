20662af7b33d531d136f4cdcad3a1e86d6743008
Edgar Mesquita <edgar.rj@gmail.com>
2022-11-05 09:36:34 +0000

    update api version

--- InvoiceExtractor/BusinessPortalClient.cs
+++ InvoiceExtractor/BusinessPortalClient.cs
@@ -13,19 +13,20 @@ public class BusinessPortalClient : IDisposable
     private readonly HttpClient _client;
     private string _sessionId;
     private readonly Guid _deviceUid;
-    private readonly long _startTimestamp;
+    private const string Version = "BP.11.61";
+    
     public BusinessPortalClient()
     {
         _client = new HttpClient();
-        _startTimestamp = DateTime.UtcNow.ToUnixTimeInSeconds();
-        _sessionId = Guid.NewGuid() + "b" + _startTimestamp;
+        var startTimestamp = DateTime.UtcNow.ToUnixTimeInSeconds();
+        _sessionId = Guid.NewGuid() + "b" + startTimestamp;
         _deviceUid = Guid.NewGuid(); //22985b26-5113-4f1f-a32d-9c11bbf1ef4c
     }
 
     public async Task<AuthenticationToken> StartAuthenticationAsync(LoginRequest loginRequest)
     {
         var response = await _client.PostAsync(
-            $"https://node.bolt.eu/business-portal/businessPortal/startAuthentication?version=BP.11.58&session_id={_sessionId}",
+            $"https://node.bolt.eu/business-portal/businessPortal/startAuthentication?version={Version}&session_id={_sessionId}",
             JsonContent.Create(new AuthenticationRequest
             {
                 DeviceUid = _deviceUid.ToString(),
@@ -44,7 +45,7 @@ public class BusinessPortalClient : IDisposable
     public async Task<RefreshTokenData> CompleteAuthenticationAsync(string sms, string verificationToken)
     {
         var confirmationResponse = await _client.PostAsync(
-            $"https://node.bolt.eu/business-portal/businessPortal/completeAuthentication?version=BP.11.58&session_id={_sessionId}",
+            $"https://node.bolt.eu/business-portal/businessPortal/completeAuthentication?version={Version}&session_id={_sessionId}",
             JsonContent.Create(new AuthenticationConfirmationRequest
             {
                 DeviceUid = _deviceUid.ToString(),
@@ -64,7 +65,7 @@ public class BusinessPortalClient : IDisposable
     public async Task<AccessTokenData> GetAccessTokenAsync(string refreshToken)
     {
         var accessTokenResponse = await _client.PostAsync(
-            $"https://node.bolt.eu/business-portal/businessPortal/getAccessToken?version=BP.11.58&session_id={_sessionId}",
+            $"https://node.bolt.eu/business-portal/businessPortal/getAccessToken?version={Version}&session_id={_sessionId}",
             JsonContent.Create(new RefreshTokenData
             {
                 RefreshToken = refreshToken
@@ -80,7 +81,7 @@ public class BusinessPortalClient : IDisposable
     public async Task<UserInfo> GetUserInfoAsync(string accessToken)
     {
         using var requestMessage = new HttpRequestMessage(HttpMethod.Get,
-            $"https://node.bolt.eu/business-portal/businessPortalUser/getUserInfo/?version=BP.11.58&session_id={_sessionId}");
+            $"https://node.bolt.eu/business-portal/businessPortalUser/getUserInfo/?version={Version}&session_id={_sessionId}");
         requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
 
         var userResponse = await _client.SendAsync(requestMessage);
@@ -94,7 +95,7 @@ public class BusinessPortalClient : IDisposable
     public async Task<RideListData> GetRiderPageAsync(string accessToken, int companyId, int page, int limit = 100)
     {
         using var requestMessage = new HttpRequestMessage(HttpMethod.Get,
-            $"https://node.bolt.eu/business-portal/businessPortal/getRidesHistory/?version=BP.11.58&session_id={_sessionId}&company_id={companyId}&limit={limit}&page={page}");
+            $"https://node.bolt.eu/business-portal/businessPortal/getRidesHistory/?version={Version}&session_id={_sessionId}&company_id={companyId}&limit={limit}&page={page}");
         requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
 
         var listResponse = await _client.SendAsync(requestMessage);
@@ -121,6 +122,10 @@ public class BusinessPortalClient : IDisposable
     {
         var handler = new JwtSecurityTokenHandler();
         var jwtSecurityToken = handler.ReadJwtToken(accessToken);
+
+        var iatClaim = jwtSecurityToken.Claims
+            .FirstOrDefault(claim => claim.Type == "iat")?.Value;
+        
         var dataClaimJson = jwtSecurityToken.Claims
             .FirstOrDefault(claim => claim.Type == "data")?.Value;
 
@@ -129,7 +134,7 @@ public class BusinessPortalClient : IDisposable
 
         var dataClaim = JsonSerializer.Deserialize<DataClaim>(dataClaimJson);
         
-        if(dataClaim?.BusinessAdminUserId > 0)
-            _sessionId = $"{dataClaim.BusinessAdminUserId}b{_startTimestamp}";
+        if(dataClaim?.BusinessAdminUserId > 0 && !string.IsNullOrEmpty(iatClaim))
+            _sessionId = $"{dataClaim.BusinessAdminUserId}b{iatClaim}";
     }
 }
